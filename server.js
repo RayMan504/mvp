12d41905b13869a31740e58f4552e33a925f3d2c
RayMan504 <uraeusmillet@gmail.com>
2017-07-28 14:08:25 -0500

    (angular) implement app to use angular

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');
 var app = express();
 
 // set the port of our application
@@ -10,10 +13,14 @@ app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));          // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // set the home page route
 app.get('/', function(req, res) {
-    res.end('hello world!');
+    res.sendFile('./public/index.html');
     // ejs render automatically looks in the views folder
     // res.render('index');
 });
