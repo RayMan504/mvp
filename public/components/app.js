4d66b44d733d83cb972ff2f8ec975722a32a9cfe
RayMan504 <uraeusmillet@gmail.com>
2017-07-29 11:01:56 -0500

    (angular) add home and favorites routes

--- public/components/app.js
+++ public/components/app.js
@@ -1,4 +1,13 @@
-angular.module('karaoke-party', [])
+angular.module('karaoke-party', ['ngRoute'])
+  .config(($routeProvider) => {
+    $routeProvider
+      .when('/home', {
+        templateUrl: './templates/app.html',
+      })
+      .when('/favorites', {
+        templateUrl: './templates/favorites.html',
+      });
+  })
   .controller('AppCtrl', ($scope, $window) => {
     // show exampleSongData in app component
     $scope.currentSong = $window.exampleSongData;
--- public/components/search.js
+++ public/components/search.js
@@ -1,7 +1,10 @@
 angular.module('karaoke-party')
   .controller('SearchCtrl', ($scope, $window) => {
-    $scope.trackSearchResults = () => {
-      $scope.songs = $window.exampleSongData;
-      console.log($scope.songs,'yo');
+    $scope.trackSearchResults = (query) => {
+      if (query) {
+        $scope.songs = $window.exampleSongData;
+        console.log($scope.songs,'yo');
+      } 
+      $scope.message = 'No results'; 
     };
   });
