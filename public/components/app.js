a27a661ffae832a2b692b4732f395449c2b76d47
RayMan504 <uraeusmillet@gmail.com>
2017-07-28 19:28:40 -0500

    (nav bar) build nav bar controller

--- public/components/app.js
+++ public/components/app.js
@@ -1,1 +1,15 @@
-const myApp = angular.module('karaoke-party', []);
+angular.module('karaoke-party', [])
+  .controller('AppCtrl', ($scope) => {
+    $scope.currentNavItem = 'page1';
+  })
+
+  .directive('app', () => ({
+    // TODO
+    // make scope object
+    restrict: 'E',
+    templateUrl: 'templates/app.html',
+    controller: 'AppCtrl',
+    controllerAs: 'ctrl',
+    bindToController: true,
+
+  }));
