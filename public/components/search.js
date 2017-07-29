bc8dadd365db3deb6c542ba1350389da6764a104
RayMan504 <uraeusmillet@gmail.com>
2017-07-29 01:53:05 -0500

    (angular) render dom elements and display exampleSongData

--- public/components/search.js
+++ public/components/search.js
@@ -1,4 +1,6 @@
 angular.module('karaoke-party')
-  .controller('SearchCtrl', () => {
-    this.songs = 'page1';
+  .controller('SearchCtrl', ($scope) => {
+    $scope.trackSearchResults = () => {
+      $scope.songs = $window.exampleSongData;
+    };
   });
