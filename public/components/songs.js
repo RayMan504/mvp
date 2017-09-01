e2c496aed6a54b22858027298dab6ede067d35bb
RayMan504 <uraeusmillet@gmail.com>
2017-08-31 21:11:20 -0500

    (songs control) define $http as param

--- public/components/songs.js
+++ public/components/songs.js
@@ -1,5 +1,5 @@
 angular.module('karaoke-party')
-  .controller('SongsCtrl', ($scope, $window, musixmatch) => {
+  .controller('SongsCtrl', ($scope, $http, $window, musixmatch) => {
     // $scope.currentNavItem = 'page1';
     $scope.songs = function() {
       $scope.results = $http.get('https://api.musixmatch.com/ws/1.1', function(data) {
