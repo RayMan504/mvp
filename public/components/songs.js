48d39dadec3f4430023e5895dc71bd81f9bc86a0
RayMan504 <uraeusmillet@gmail.com>
2017-09-05 21:30:06 -0500

    (trackSearch) trackSearch works?

--- public/components/songs.js
+++ public/components/songs.js
@@ -2,9 +2,10 @@ angular.module('karaoke-party')
   .controller('SongsCtrl', ($scope, $http, $window, musixmatch) => {
     // $scope.currentNavItem = 'page1';
     $scope.musixmatch = function() {
-      $scope.results = $http.get('https://api.musixmatch.com/ws/1.1', function(data) {
-        console.log(data);
-      });
+      $scope.results = musixmatch.trackSearch('colby', () => {});
+      // $scope.results = $http.get('https://api.musixmatch.com/ws/1.1', function(data) {
+      //   console.log(data);
+      // });
       // $scope.results = ();
       // hfuhuqfhuhu
     };
