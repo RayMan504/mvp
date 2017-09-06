a3328f625ff055c0a7c1083b8832586841cd9eab
RayMan504 <uraeusmillet@gmail.com>
2017-09-05 21:33:30 -0500

    (trackSearch) trackSearch accessible

--- public/components/songs.js
+++ public/components/songs.js
@@ -1,13 +1,13 @@
 angular.module('karaoke-party')
   .controller('SongsCtrl', ($scope, $http, $window, musixmatch) => {
     // $scope.currentNavItem = 'page1';
-    $scope.musixmatch = function() {
-      $scope.results = musixmatch.trackSearch('colby', () => {});
-      // $scope.results = $http.get('https://api.musixmatch.com/ws/1.1', function(data) {
-      //   console.log(data);
-      // });
-      // $scope.results = ();
-      // hfuhuqfhuhu
-    };
+    $scope.musixmatch = musixmatch.trackSearch('colby', () => {});
+    // $scope.results = 
+    // $scope.results = $http.get('https://api.musixmatch.com/ws/1.1', function(data) {
+    //   console.log(data);
+    // });
+    // $scope.results = ();
+    // hfuhuqfhuhu
+    // };
   })
 ;
