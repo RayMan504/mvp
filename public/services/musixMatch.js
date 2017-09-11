6b14b1037204e480b600510c334e47481655c720
RayMan504 <uraeusmillet@gmail.com>
2017-09-11 16:25:53 -0500

    (api call) api call retreives data

--- public/services/musixMatch.js
+++ public/services/musixMatch.js
@@ -8,24 +8,23 @@ angular.module('karaoke-party')
         apikey: $window.MUSIXMATCH_API_KEY,
       };
       // $http.defaults.headers.common.Authorization = 'Basic YmVlcDpib29w';
-      $http.get('https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1', {
-        params: {
-          q: obj.q,
-          apikey: obj.apikey,
-        },
-      })
-        .then(function({ data }) {
-          console.log();
-          if (callback) {
-            callback(data.items);
-          }
-        })
-        .catch(function({ data }) {
-          console.error(data);
-          // data.error.errors.forEach((err) => {
-          //   console.error(err.message);
-          // });
+      const url = `https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=JSON_CALLBACK&q_track=${obj.q}&quorum_factor=1&apikey=${obj.apikey}`;
+      $http.jsonp(url, { jsonpCallbackParam: 'callback' })
+        .success((data) => {
+          console.log(data, 'hey');
         });
+      // .then(function({ data }) {
+      //   console.log();
+      //   if (callback) {
+      //     callback(data.items);
+      //   }
+      // })
+      // .catch(function({ data }) {
+      // console.error(data);
+      // data.error.errors.forEach((err) => {
+      //   console.error(err.message);
+      // });
+      // });
     };
     // this.tracks.lyrics.get = (query, callback) => {
     //   const obj = {
