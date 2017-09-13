da816f7b30d2c35183329b9c55ebd8f2aa208be5
RayMan504 <uraeusmillet@gmail.com>
2017-09-13 12:35:36 -0500

    (deployment) update deployed version

--- public/services/musixMatch.js
+++ public/services/musixMatch.js
@@ -5,7 +5,7 @@ angular.module('karaoke-party')
       // console.log('hey bruh');
       const obj = {
         q: query,
-        apikey: $window.MUSIXMATCH_API_KEY,
+        apikey: '18e2e9aa66c7dc3985df593bf10c44b4',
       };
       // $http.defaults.headers.common.Authorization = 'Basic YmVlcDpib29w';
       const url = `https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=JSON_CALLBACK&q_track=${obj.q}&quorum_factor=1&apikey=${obj.apikey}`;
@@ -17,7 +17,7 @@ angular.module('karaoke-party')
     this.getLyrics = (trackId, callback) => {
       const obj = {
         q: trackId,
-        apikey: $window.MUSIXMATCH_API_KEY,
+        apikey: '18e2e9aa66c7dc3985df593bf10c44b4',
       };
       const url = `https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=JSON_CALLBACK&track_id=${obj.q}&apikey=${obj.apikey}`;
       $http.jsonp(url, { jsonpCallbackParam: 'callback' })
