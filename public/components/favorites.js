27970236c0f9f36c570d35f4faac2be918312309
RayMan504 <uraeusmillet@gmail.com>
2017-09-25 07:42:14 -0500

    (favorites) cannot render favorite songs. get request sends database data back to client

--- public/components/favorites.js
+++ public/components/favorites.js
@@ -1,4 +1,18 @@
 angular.module('karaoke-party')
-  .controller('FavoritesCtrl', () => {
-
+  .controller('FavoritesCtrl', ($scope, $http) => {
+    $http.get('/favorites')
+      .then((response) => {
+        // First function handles success
+        $scope.content = response.data;
+      }, (response) => {
+        // Second function handles error
+        $scope.content = 'Something went wrong';
+      });
+    $scope.stringify = function (value) {
+      console.log(value, 'undefined');
+      return JSON.stringify(value);
+    };
+    $scope.parseJson = function (json) {
+      return JSON.parse(json);
+    };
   });
