bbf95391e1b689c3bab841e9a678bc8dcb3a935c
RayMan504 <uraeusmillet@gmail.com>
2017-10-13 15:49:39 -0500

    (images) search result images are same size

--- public/services/geniusService.js
+++ public/services/geniusService.js
@@ -4,7 +4,7 @@ angular.module('karaoke-party')
     this.trackSearch = function (query, callback) {
       const obj = {
         q: query,
-        apikey: 'jQYhxGM7PD8x_hhV-_GV-Oj_B28uy-Tq3ZU6-sUTD5A6Zh_1KdBjEOD_hkNDLMEj',
+        apikey: $window.GENIUS_ACCESS_TOKEN,
       };
       const url = `https://api.genius.com/search?q=${obj.q}&access_token=${obj.apikey}`;
       $http.get(url)
@@ -16,7 +16,7 @@ angular.module('karaoke-party')
       const obj = {
         q: query,
         t: type,
-        accessToken: 'BQCVY1KPrS6pGOkBIfvpmyWWRNbzaVvIFjx6FWBWiZLSPmtfaFq9t34uwT9zaId6M9ZDhJLkp-R5bZB99P2sf6HpHGdAYHiqN9Xl7WjJjGr4h2E51Ru_wQ64dLJVRt_Czo4xBUqXN6cqUhcUhEzQpNUQIxlhpc_i1eyFOcI',
+        accessToken: $window.SPOTIFY_ACCESS_TOKEN,
       };
       const url = `https://api.spotify.com/v1/search?q=${obj.q}&type=${obj.t}&access_token=${obj.accessToken}`;
       $http.get(url).success((data) => {
